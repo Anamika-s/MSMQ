7f2f3787ea9cc1273a04863eb75ea090db51ce96
Anamika Sawhney <anamika.sawhney22@gmail.com>
2019-03-28 12:11:47 +0530

    INIT

---
+++ MSMQ_DEMO/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Messaging;
+class Program
+{
+static void Main()
+{
+    Console.WriteLine("Latest Message");
+
+    MessageQueue MyQueue=null;
+    if (MessageQueue.Exists(@".\Private$\MyQueue"))
+    {
+
+        Message MyMessage = MyQueue.Receive();
+        // Set FOrmatter for Messge
+
+        MyMessage.Formatter = new BinaryMessageFormatter();
+        Console.WriteLine(MyMessage.Body.ToString());
+        Console.Read();
+    }
+}
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Messaging;
+//Sender
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter Message to be sent");
+        Console.WriteLine("Please Enter HP: to enter high priority Messages");
+        string MessageToBeSend = Console.ReadLine();
+
+        //Get or Create Queue
+        MessageQueue MyQueue = null ;
+        if (MessageQueue.Exists(@".\Private$\MyQueue"))
+        {
+            MyQueue = new MessageQueue(@".\Private$\MyQueue");
+        }
+        else
+        {
+            MyQueue = MessageQueue.Create(@".\Private$\MyQueue");
+        }
+
+        //Create Message
+
+        IMessage MyMessage = new System.Messaging.Message();
+
+        // Set FOrmatter for Messge
+
+        MyMessage.Formatter = new BinaryMessageFormatter();
+        MyMessage.Body = MessageToBeSend;
+        MyMessage.Label = "App1Message";
+
+        if (MessageToBeSend.Contains("HP:"))
+        {
+            MyMessage.Priority = MessagePriority.Highest;
+        }
+        else
+        {
+            MyMessage.Priority = MessagePriority.Normal;
+
+        }
+        MyQueue.Send(MyMessage);
+        Console.WriteLine("Thanks for Sending Message");
+        Conosle.Read();
+    }
+}
+}
